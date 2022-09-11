9968d340ce265ee403650178554ac946c19fcedb
jimsharples <jamie.sharples@gmail.com>
2022-09-11 15:56:49 +1000

    Fixed export for enums

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,10 +1,13 @@
-export type { OperationResult, CollectionConfig, IndexEntry, IndexVersion, IndexContent, IndexConfig, IndexEntryFieldNameValue, BufferType } from './db/Types';
+export type { OperationResult, CollectionConfig, IndexEntry, IndexVersion, IndexContent, IndexConfig, IndexEntryFieldNameValue } from './db/Types';
 export type { ObjectBuffer, BufferEntry } from './db/buffer/ObjectBuffer';
 export type { Collection } from './db/collection/Collection';
 export type { Cursor } from './db/cursor/Cursor';
 export type { SearchCursor } from './db/cursor/SearchCursor';
 export type { SortedCursor } from './db/cursor/SortedCursor';
-export type { SearchItem, SearchItemComparison } from './db/search/SearchTypes';
-export type { SortOrderType, SortOrderItem } from './db/sort/SortTypes';
+export type { SearchItem } from './db/search/SearchTypes';
+export type { SortOrderItem } from './db/sort/SortTypes';
 export { InvalidConfiguration, MissingConfiguration, DuplicateKey } from './db/Types';
 export { DB } from './db/DB';
+export { BufferType } from './db/Types';
+export { SearchItemComparison } from './db/search/SearchTypes';
+export { SortOrderType } from './db/sort/SortTypes';
--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.DB = exports.DuplicateKey = exports.MissingConfiguration = exports.InvalidConfiguration = void 0;
+exports.SortOrderType = exports.SearchItemComparison = exports.BufferType = exports.DB = exports.DuplicateKey = exports.MissingConfiguration = exports.InvalidConfiguration = void 0;
 var Types_1 = require("./db/Types");
 Object.defineProperty(exports, "InvalidConfiguration", { enumerable: true, get: function () { return Types_1.InvalidConfiguration; } });
 Object.defineProperty(exports, "MissingConfiguration", { enumerable: true, get: function () { return Types_1.MissingConfiguration; } });
 Object.defineProperty(exports, "DuplicateKey", { enumerable: true, get: function () { return Types_1.DuplicateKey; } });
 var DB_1 = require("./db/DB");
 Object.defineProperty(exports, "DB", { enumerable: true, get: function () { return DB_1.DB; } });
+var Types_2 = require("./db/Types");
+Object.defineProperty(exports, "BufferType", { enumerable: true, get: function () { return Types_2.BufferType; } });
+var SearchTypes_1 = require("./db/search/SearchTypes");
+Object.defineProperty(exports, "SearchItemComparison", { enumerable: true, get: function () { return SearchTypes_1.SearchItemComparison; } });
+var SortTypes_1 = require("./db/sort/SortTypes");
+Object.defineProperty(exports, "SortOrderType", { enumerable: true, get: function () { return SortTypes_1.SortOrderType; } });
 //# sourceMappingURL=index.js.map
